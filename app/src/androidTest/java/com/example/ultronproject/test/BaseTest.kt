f459fc95607990b64fe3d9767a5675ad985ff978
Alexey Shihalev <alesha.shikhalev@mail.ru>
2025-05-23 23:09:03 +0300

    refactor: add base test and allure steps

--- app/src/androidTest/java/com/example/ultronproject/test/BaseTest.kt
+++ app/src/androidTest/java/com/example/ultronproject/test/BaseTest.kt
@@ -1,4 +1,26 @@
-package com.example.ultronproject.test
+package com.example.ultronproject.test;
 
-class BaseTest {
+import com.atiurin.ultron.allure.config.UltronAllureConfig
+import com.atiurin.ultron.allure.listeners.DetailedOperationAllureListener
+import com.atiurin.ultron.allure.listeners.ScreenshotAttachListener
+import com.atiurin.ultron.allure.listeners.WindowHierarchyAttachListener
+import com.atiurin.ultron.core.compose.config.UltronComposeConfig
+import com.atiurin.ultron.core.config.UltronCommonConfig
+import com.atiurin.ultron.core.config.UltronConfig
+import org.junit.BeforeClass
+
+open class BaseTest {
+    companion object {
+        @BeforeClass
+        @JvmStatic
+        fun config() {
+            UltronConfig.applyRecommended()
+            UltronAllureConfig.applyRecommended()
+            UltronComposeConfig.applyRecommended()
+            UltronCommonConfig.addListener(ScreenshotAttachListener())
+            UltronCommonConfig.addListener(WindowHierarchyAttachListener())
+            UltronCommonConfig.addListener(DetailedOperationAllureListener())
+            //UltronAllureConfig.setAllureResultsDirectory("build/allure-results")
+        }
+    }
 }
--- app/src/androidTest/java/com/example/ultronproject/test/ClickerTest.kt
+++ app/src/androidTest/java/com/example/ultronproject/test/ClickerTest.kt
@@ -1,12 +1,14 @@
 package com.example.ultronproject.test
 
+import com.atiurin.ultron.allure.step.step
 import com.atiurin.ultron.core.compose.createDefaultUltronComposeRule
 import com.example.ultronproject.test.Pages.ClickerScreenPage
 import com.example.ultronproject.ui.screens.ClickerScreen
 import org.junit.Rule
 import org.junit.Test
 
-class ClickerTest {
+
+class ClickerTest : BaseTest() {
     @get:Rule
     val composeRule = createDefaultUltronComposeRule()
 
@@ -14,15 +16,24 @@ class ClickerTest {
     fun clickerIncrementTest() {
         val clickerScreen = ClickerScreenPage()
 
-        composeRule.setContent {
-            ClickerScreen()
+        step("Открываем экран") {
+            composeRule.setContent {
+                ClickerScreen()
+            }
         }
 
-        clickerScreen.assertCounterEquals(0)
-        clickerScreen.clickIncrementButton()
-        clickerScreen.assertCounterEquals(1)
-        clickerScreen.clickIncrementButton()
-        clickerScreen.assertCounterEquals(2)
-    }
+        step("Проверяем счётчик 0") {
+            clickerScreen.assertCounterEquals(0)
+        }
 
+        step("Нажимаем кнопку → счётчик 1") {
+            clickerScreen.clickIncrementButton()
+            clickerScreen.assertCounterEquals(1)
+        }
+
+        step("Нажимаем кнопку → счётчик 2") {
+            clickerScreen.clickIncrementButton()
+            clickerScreen.assertCounterEquals(2)
+        }
+    }
 }
